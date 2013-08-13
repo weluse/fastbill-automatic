5b7cb8175050989d1a49bc1e322a121b39729309
Raphael Randschau <rrandschau@weluse.de>
2013-08-13 07:51:17 +0200

    stub the login and build_request methods

--- lib/fastbill-automatic/client.rb
+++ lib/fastbill-automatic/client.rb
@@ -2,9 +2,28 @@ module FastbillAutomatic
   class Client
     SERVICE_URL = "https://my.fastbill.com/api/1.0/api.php"
 
-    attr_accessor :config
-    def initialize config
-      @config = config
+    attr_accessor :email, :api_key
+    def initialize email, api_key = nil
+      @email = email
+      @api_key = api_key
+    end
+
+    def login_request password
+      return build_request({}, {
+        'X-Username' => email,
+        'X-Password' => password
+        })
+    end
+
+    def build_request options, headers = {}
+      return ::Typhoeus::Request.new(
+        SERVICE_URL, options.merge({
+            :method => :post,
+            :headers => {
+              'Content-Type' => 'application/json'
+            }.merge(headers)
+          })
+        )
     end
   end
 end
--- test/fastbill-automatic/client_test.rb
+++ test/fastbill-automatic/client_test.rb
@@ -4,4 +4,18 @@ class ClientTest < MiniTest::Unit::TestCase
 	def test_correct_service_url
     assert_equal "https://my.fastbill.com/api/1.0/api.php", ::FastbillAutomatic::Client::SERVICE_URL
 	end
+
+  def test_handles_user_login
+    response = Typhoeus::Response.new(code: 200, body: "{'apikey' : 'paul'}")
+    Typhoeus.stub('my.fastbill.com').and_return(response)
+
+    client = ::FastbillAutomatic::Client.new("max@mustermann.de")
+    assert client.login("nopass123")
+
+    assert_equal client.api_key, "paul"
+  end
+
+  def test_passes_content_type_json
+    client = ::FastbillAutomatic::Client.new("max@mustermann.de", "nopass123")
+  end
 end
