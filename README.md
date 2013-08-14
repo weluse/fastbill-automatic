8d844572a1e1a8124f0753674f1a5066e4784305
Raphael Randschau <rrandschau@weluse.de>
2013-08-14 15:38:44 +0200

    Update documentation

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
-fastbill-automatic
-==================
+# fastbill-automatic
+
 <!---
 [![Gem Version](https://badge.fury.io/rb/fastbill-automatic.png)](http://badge.fury.io/rb/fastbill-automatic)
 [![Build Status](https://travis-ci.org/weluse/fastbill-automatic.png?branch=master)](https://travis-ci.org/weluse/fastbill-automatic)
 -->
 
-Ruby wrapper for the FastBill Automatic API
+Ruby wrapper for the FastBill Automatic API, Version 1.3 (15.01.2013).
 
 ## Integration
 
@@ -23,6 +23,55 @@ $ gem install fastbill-automatic
 
 ## Usage
 
+Before working with `Customer`, `Invoice` etc. you have to create a working client instance:
+
+``` ruby
+require 'fastbill-automatic'
+::FastbillAutomatic.client = Client.new("<max@mustermann.de>", "<apikey>")
+```
+
+Afterwards you can easily list and search for existing Customers:
+
+``` ruby
+# retrieve all customers
+all_customers = ::FastbillAutomatic::Customer.all()
+
+# search for customers by city
+customers_in_paris = ::FastbillAutomatic::Customer.all({ city: 'Paris' })
+
+# load a specific Customer
+the_customer = ::FastbillAutomatic::Customer.find_by_id(42)
+```
+
+Create, edit, update and delete Customers:
+
+``` ruby
+# update an existing Customer
+any_customer = ::FastbillAutomatic::Customer.all().sample
+any_customer.first_name = 'Max'
+any_customer.last_name = 'Mustermann'
+any_customer.save # automatically calls Customer#update
+
+# create a new customer
+new_customer = ::FastbillAutomatic::Customer.new({
+  first_name: 'Maxi',
+  last_name: 'Musterfrau'
+})
+new_customer.save # automatically calls Customer#create
+
+# remove a customer from FastbillAutomatic
+new_customer.destroy
+```
+
+Customers need to fullfil some constraints. If you make invalid requests you can access the error messages using `#error`
+
+``` ruby
+new_customer = ::FastbillAutomatic::Customer.new({
+  customer_type: 'business'
+})
+new_customer.save # => false
+new_customer.errors # => ['Organization is missing', 'Zipcode is missing', 'City is missing', 'Country_Code is missing']
+```
 
 ## Development
 
--- lib/fastbill-automatic/client.rb
+++ lib/fastbill-automatic/client.rb
@@ -2,6 +2,7 @@ require 'json'
 require 'typhoeus'
 
 module FastbillAutomatic
+  #
   class Response
     attr_reader :body
     def initialize success, body
@@ -22,15 +23,36 @@ module FastbillAutomatic
     end
   end
 
+  # Executes requests against the FastbillAutomatic API.
+  #
+  # When creating instances of Client you need to pass along your current
+  # FastbillAutomatic email address, as well as a valid api_key.
   class Client
     SERVICE_URL = "https://automatic.fastbill.com/api/1.0/api.php"
 
-    attr_accessor :email, :api_key
+    # The FastbillAutomatic email address used to log in with FastbillAutomatic
+    attr_reader :email
+
+    # The FastbillAutomatic api_key as displayed in your FastbillAutomatic profil page
+    attr_reader :api_key
+
+    # Creates a new instance of ::FastbillAutomatic::Client, assigning both
+    # :email and :api_key.
+    #
+    # Most likely you'll want to assign the returned instance to
+    # ::FastbillAutomatic.client to use it as the default client.
+    #
+    # Alternativly you can pass the client as an argument to most
+    # methods that execute network requests.
     def initialize email, api_key
       @email = email
       @api_key = api_key
     end
 
+    # Synchronously executes a request against the FastbillAutomatic API using
+    # Typhoeus.
+    #
+    # Responses are wrapped using ::FastbillAutomatic::Response
     def execute_request service, other = {}, headers = {}
       request = build_request(service, other, headers)
       response = request.run
@@ -43,6 +65,9 @@ module FastbillAutomatic
       return Response.new(response.success?, JSON.parse(response.body))
     end
 
+    # When you need more control over each request you can use this method
+    # to receive a ::Typhoeus::Request instance which you manually need to
+    # execute.
     def build_request service, other = {}, headers = {}
       return build_typhoeus_request({
           body: JSON.dump({
@@ -50,6 +75,8 @@ module FastbillAutomatic
           }.merge(other))
         }, headers)
     end
+
+    protected
 
     def build_typhoeus_request options, headers = {}
       return ::Typhoeus::Request.new(
