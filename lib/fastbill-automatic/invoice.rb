4ffafa682bcbeb5f9ae0dfe043b606c8c748cf3f
Raphael Randschau <rrandschau@weluse.de>
2013-08-14 16:06:41 +0200

    WIP Invoice.all

--- lib/fastbill-automatic/invoice.rb
+++ lib/fastbill-automatic/invoice.rb
@@ -1,4 +1,7 @@
 module FastbillAutomatic
+  # The Invoice class wraps basic interactions for invoice.get|create|update|delete service calls.
   class Invoice
+    def self.all filters = {}, client = FastbillAutomatic.client
+    end
   end
 end
