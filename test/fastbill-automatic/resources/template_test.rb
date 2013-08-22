f41f42d0bb9af6470f0b0ed2344ac7a91b0ce35a
Raphael Randschau <rrandschau@weluse.de>
2013-08-22 12:27:01 +0200

    add article, subscription and template test stubs

---
+++ test/fastbill-automatic/resources/template_test.rb
@@ -0,0 +1,11 @@
+require 'test_helper'
+
+class TemplateTest < Minitest::Test
+  attr_accessor :client
+  def setup
+    @client = ::FastbillAutomatic.client = ::FastbillAutomatic::Client.new("max@mustermann.de", "nopass123")
+  end
+  def teardown
+    ::FastbillAutomatic.client = nil
+  end
+end
